dae0588c79b60bd87f52b707e92b8473de1a5210
0joong <162318623+0joong@users.noreply.github.com>
2025-05-11 18:06:22 +0900

    Update danawaCrawler.py

--- danawaCrawler.py
+++ danawaCrawler.py
@@ -7,6 +7,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException, NoSuchElementException
+import chromedriver_autoinstaller
 
 class DanawaCrawler:
     def __init__(self, headless=True, logger=None):
